c3e02a3416e7627e017ad8286b2bc3446b838898
TaliaMoffat <92391649+TaliaMoffat@users.noreply.github.com>
2022-01-19 16:34:08 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Main {
+
+  //welcome method
+
+  public static void main(String[] args) {
+    //call welcome method
+    //start loop while playing
+    //call who goes first method
+    //run turns
+    //call calculating score method?
+    //print winner and scores
+    //offer another game
+    //close while playing loop
+    //good bye message
+  }
+}
