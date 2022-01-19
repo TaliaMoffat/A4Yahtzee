6ff0dab8349daa268518e7f3b77452b1104085fe
Talia Moffat <92391649+TaliaMoffat@users.noreply.github.com>
2022-01-19 17:25:41 +0000

    1 hour in

--- Main.java
+++ Main.java
@@ -2,17 +2,77 @@ import java.util.*;
 
 class Main {
 
-  //welcome method
+  //static variables
+  Scanner input = new Scanner(System.in);
+  boolean isPlaying = false;
+  boolean validInput = false;
+  int userInt = 0; 
+
+  public static void welcomeUser(){
+    System.out.println("Welcome to our Yahtzee game! We are so excited to play with you, but first you need to answer some questions.");
+    questionOne();
+    questionTwo();
+    System.out.println("Okay! Let's play!");
+  }
+
+  public static void invalidInput(){
+    System.out.println("That doesn't look right, try again.");
+  }
+
+  public void questionOne(){
+    while (validInput = false){
+      System.out.println("Number 1: Do you want to play? Type 1 for yes or 2 for no.");
+      try{
+        userInt = input.nextInt();
+      } catch (NumberFormatException exception){
+          invalidInput();
+          continue;
+      }
+      if (userInt == 1){
+        isPlaying = true;
+        validInput = true;
+        break;
+        } else if (userInt == 2){
+          isPlaying = false;
+          validInput = true;
+          break;
+        } else if (userInt != 1 && userInt != 2){
+          invalidInput();
+        }
+    }
+  }
+
+  public void questionTwo(){
+    while (validInput = false){
+      System.out.println("Number 2: Do you know how to play? Type 1 for yes or 2 for no.");
+      try{
+        userInt = input.nextInt();
+      } catch (NumberFormatException exception){
+          invalidInput();
+          continue;
+      }
+      if (userInt == 1){
+        validInput = true;
+        break;
+        } else if (userInt == 2){
+          validInput = true;
+          //call rules method
+          break;
+        } else if (userInt != 1 && userInt != 2){
+          invalidInput();
+        }
+    }
+  }
 
   public static void main(String[] args) {
-    //call welcome method
-    //start loop while playing
+    welcomeUser();
+    while (isPlaying = true){
     //call who goes first method
     //run turns
     //call calculating score method?
     //print winner and scores
     //offer another game
-    //close while playing loop
+    }
     //good bye message
   }
 }
---
+++ Talia.md
@@ -0,0 +1,6 @@
+# Notes on methods
+## Welcome user method
+* greeting
+* call questions
+  * Q1 wanna play?
+  * Q2 know how to?
